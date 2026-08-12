920f868cfadfd4cfd528422b734775b4fe10b5f5
Christopher Martin <christophermartin13193@gmail.com>
2026-08-12 20:42:54 +0200

    Fixed an error in exec_once

--- pro/.config/hypr/required/exec_once.lua
+++ pro/.config/hypr/required/exec_once.lua
@@ -2,5 +2,5 @@
 
 hl.on("hyprland.start", function ()
 	hl.exec_cmd("hyprpaper")
-	hl.exec_cmd("waybar -c ~/.config/waybar/bottom/.config.jsonc -s ~/.config/waybar/bottom/sytle.css")
+	hl.exec_cmd("waybar -c ~/.config/waybar/bottom/config.jsonc -s ~/.config/waybar/bottom/style.css")
 end)
